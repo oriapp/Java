03128a12a9e72cb07fa7710c4b7e1f455c1e3410
R C N <plbwymw@gmail.com>
2021-05-13 00:07:48 +0300

    Test

--- src/com/ori/cmd.java
+++ src/com/ori/cmd.java
@@ -1,2 +1,18 @@
-package com.ori;public class cmd {
+package com.ori;
+import java.io.*;
+
+public class cmd {
+
+        public static void main(String[] args) throws Exception {
+//            Process runtime = Runtime.getRuntime().exec("cmd /c start cmd.exe");
+            try
+            {
+                Runtime.getRuntime().exec("cmd /c start cmd.exe /K \" git push -u origin main\"");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Something went wrong \\:");
+                e.printStackTrace();
+            }
+        }
 }
