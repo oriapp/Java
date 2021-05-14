83122125404cf69c8cc9292a8951d3d331358d0f
R C N <plbwymw@gmail.com>
2021-05-14 23:11:56 +0300

    sql testing

---
+++ src/com/ori/Sql.java
@@ -0,0 +1,16 @@
+package com.ori;
+
+import java.sql.*;
+
+public class Sql {
+
+    static final String DB_URL = "jdbc:mysql://localhost/JDB";
+    static final String USER = "root";
+    static final String PASS = "";
+    static final String QUERY = "SELECT id FROM Test";
+
+    public static void main(String[] argus){
+
+    }
+
+}
